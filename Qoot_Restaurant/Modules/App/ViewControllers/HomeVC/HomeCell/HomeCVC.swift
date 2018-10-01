1f381841df010bec4ba742d155560aef32eb5fe9
Bibin Mathew <bibin.m@codelynks.com>
2018-10-01 07:14:31 +0530

    Home age ui implementation.

---
+++ Qoot_Restaurant/Modules/App/ViewControllers/HomeVC/HomeCell/HomeCVC.swift
@@ -0,0 +1,21 @@
+//
+//  HomeCVC.swift
+//  Qoot_Restaurant
+//
+//  Created by Bibin Mathew on 10/1/18.
+//  Copyright © 2018 Cl. All rights reserved.
+//
+
+import UIKit
+
+class HomeCVC: UICollectionViewCell {
+    @IBOutlet weak var iconImageView: UIImageView!
+    @IBOutlet weak var headingLabel: UILabel!
+    @IBOutlet weak var subHeadingLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
--- Qoot_Restaurant/Modules/App/ViewControllers/HomeVC/HomeVC.swift
+++ Qoot_Restaurant/Modules/App/ViewControllers/HomeVC/HomeVC.swift
@@ -9,18 +9,27 @@
 import UIKit
 
 class HomeVC: BaseViewController {
-
+    @IBOutlet weak var statusLabel: UILabel!
+    @IBOutlet weak var statusSwitch: UISwitch!
+    @IBOutlet weak var homeCollectionView: UICollectionView!
     override func initView() {
         super.initView()
         initialisation()
+        localization()
     }
     
     func initialisation(){
-        self.title = "Qoot".localiz()
         addingLeftBarButton()
         self.leftButton?.setImage(UIImage(named: "hamburger"), for: UIControlState.normal)
-       
-        //setUpCollectionView()
+        setUpCollectionView()
+    }
+    
+    func localization(){
+      self.title = "Qoot".localiz()
+    }
+    
+    func setUpCollectionView(){
+        homeCollectionView.register(UINib(nibName: "HomeCVC", bundle: nil), forCellWithReuseIdentifier:"homeCell" )
     }
     
     override func leftNavButtonAction() {
@@ -31,6 +40,8 @@ class HomeVC: BaseViewController {
             self.slideMenuController()?.openRight()
         }
     }
+    
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
@@ -48,4 +59,37 @@ class HomeVC: BaseViewController {
     }
     */
 
+}
+
+extension HomeVC:UICollectionViewDataSource,UICollectionViewDelegate,UICollectionViewDelegateFlowLayout{
+    
+    //MARK- Collection View Datasources
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+      
+        return 10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell:HomeCVC = collectionView.dequeueReusableCell(withReuseIdentifier: "homeCell", for: indexPath) as! HomeCVC
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize{
+        return CGSize(width: (collectionView.frame.size.width - 10)/2, height: 80)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 10
+    }
+    
+    // MARK: Collection Cell Delegates
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        
+    }
 }
